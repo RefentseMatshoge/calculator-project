de25e59b295e09522ed001d2ce2308da37d89e2e
Refentse Matshoge <refentsefentse49@gmail.com>
2026-07-24 17:15:05 +0200

    Add operator selection functionality to calculator

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let display = document.getElementById("display");
 
+let firstNumber = "";
+let operator = "";
+
 function appendValue(value) {
     display.value += value;
 }
 
 function clearDisplay() {
+    display.value = "";
+}
+
+function chooseOperator(op) {
+    firstNumber = display.value;
+    operator = op;
     display.value = "";
 }
